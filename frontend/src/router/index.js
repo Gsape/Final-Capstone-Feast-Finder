ab5cffe8c67d0ca3ebf07cbcdfbbe75115d731c2
Grant <grant.sapir@gmail.com>
2021-12-08 09:38:57 -0500

    question view

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,6 +5,7 @@ import Login from '../views/Login.vue'
 import Logout from '../views/Logout.vue'
 import Register from '../views/Register.vue'
 import store from '../store/index'
+import Questionnaire from '../views/Questionnaire.vue'
 
 Vue.use(Router)
 
@@ -53,6 +54,15 @@ const router = new Router({
         requiresAuth: false
       }
     },
+    {
+      path: "/questionnaire",
+      name: "questionnaire",
+      component: Questionnaire, //what is this and why?
+      meta: {
+        requiresAuth: false
+      }
+    }
+    
   ]
 })
 
