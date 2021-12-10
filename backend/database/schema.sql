b8dccd865b10503bc8212046ba0f38e4515974a7
Paige Wortmann <pagewortmann@gmail.com>
2021-12-10 11:17:21 -0500

    Updated feastForm method

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,6 +1,6 @@
 --BEGIN TRANSACTION;
 --
---DROP TABLE IF EXISTS users;
+--DROP TABLE IF EXISTS users, restaurants, cuisine, user_restaurants, user_cuisine;
 --DROP SEQUENCE IF EXISTS seq_user_id;
 --
 --CREATE SEQUENCE seq_user_id
@@ -11,12 +11,52 @@
 --
 --
 --CREATE TABLE users (
---	user_id int DEFAULT nextval('seq_user_id'::regclass) NOT NULL,
---	username varchar(50) NOT NULL,
---	password_hash varchar(200) NOT NULL,
---	role varchar(50) NOT NULL,
---	CONSTRAINT PK_user PRIMARY KEY (user_id)
+--   user_id int DEFAULT nextval('seq_user_id'::regclass) NOT NULL,
+--   username varchar(50) NOT NULL,
+--   password_hash varchar(200) NOT NULL,
+--   role varchar(50) NOT NULL,
+--   CONSTRAINT PK_user PRIMARY KEY (user_id)
 --);
+--
+--CREATE TABLE restaurants (
+--        restaurant_id SERIAL,
+--        yelp_id VARCHAR (255),
+--        restaurant_name VARCHAR (255),
+--        is_shown BOOLEAN,
+--
+--        PRIMARY KEY (restaurant_id)
+--);
+
+--CREATE TABLE cuisine (
+--        cuisine_id SERIAL,
+--        cuisine_type VARCHAR (255),
+--
+--        PRIMARY KEY (cuisine_id)
+--);
+--
+--CREATE TABLE user_restaurants (
+--        user_id SERIAL,
+--        restaurant_id SERIAL,
+--
+--        PRIMARY KEY (user_id, restaurant_id),
+--        FOREIGN KEY (user_id) REFERENCES users (user_id),
+--        FOREIGN KEY (restaurant_id) REFERENCES restaurants (restaurant_id)
+--);
+--
+--CREATE TABLE user_cuisine (
+--        user_id SERIAL,
+--        cuisine_id SERIAL,
+--
+--        PRIMARY KEY (user_id, cuisine_id),
+--        FOREIGN KEY (user_id) REFERENCES users (user_id),
+--        FOREIGN KEY (cuisine_id) REFERENCES cuisine (cuisine_id)
+--);
+--
+--ALTER TABLE users
+--ADD COLUMN radius NUMERIC;
+--
+--ALTER TABLE users
+--ADD COLUMN zip_code NUMERIC;
 --
 --INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 --INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
@@ -24,7 +64,7 @@
 --
 --COMMIT TRANSACTION;
 
-BEGIN TRANSACTION;
+BEGIN TRANSACTION
 
 DROP TABLE IF EXISTS users, restaurants, cuisine, user_restaurants, user_cuisine;
 DROP SEQUENCE IF EXISTS seq_user_id;
@@ -37,20 +77,11 @@ CREATE SEQUENCE seq_user_id
 
 
 CREATE TABLE users (
-	user_id int DEFAULT nextval('seq_user_id'::regclass) NOT NULL,
-	username varchar(50) NOT NULL,
-	password_hash varchar(200) NOT NULL,
-	role varchar(50) NOT NULL,
-	CONSTRAINT PK_user PRIMARY KEY (user_id)
-);
-
-CREATE TABLE restaurants (
-        restaurant_id SERIAL,
-        yelp_id VARCHAR (255),
-        restaurant_name VARCHAR (255),
-        is_shown BOOLEAN,
-
-        PRIMARY KEY (restaurant_id)
+   user_id int DEFAULT nextval('seq_user_id'::regclass) NOT NULL,
+   username varchar(50) NOT NULL,
+   password_hash varchar(200) NOT NULL,
+   role varchar(50) NOT NULL,
+   CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
 CREATE TABLE cuisine (
@@ -62,11 +93,11 @@ CREATE TABLE cuisine (
 
 CREATE TABLE user_restaurants (
         user_id SERIAL,
-        restaurant_id SERIAL,
+        yelp_id VARCHAR (255),
+        is_shown BOOLEAN,
 
-        PRIMARY KEY (user_id, restaurant_id),
-        FOREIGN KEY (user_id) REFERENCES users (user_id),
-        FOREIGN KEY (restaurant_id) REFERENCES restaurants (restaurant_id)
+        PRIMARY KEY (user_id),
+        FOREIGN KEY (user_id) REFERENCES users (user_id) -- <--- there was a coma right here
 );
 
 CREATE TABLE user_cuisine (
@@ -87,5 +118,5 @@ ADD COLUMN zip_code NUMERIC;
 INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
 INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
 
+COMMIT TRANSACTION
 
-COMMIT TRANSACTION;
