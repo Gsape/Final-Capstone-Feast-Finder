2730db18b225f4ab717a1d29c890759c97baf66c
Elisabeth Udyawar <elisabeth.bass@gmail.com>
2021-12-11 14:46:40 -0500

    yelp api key call created and working

--- backend/src/main/java/com/techelevator/controller/AuthenticationController.java
+++ backend/src/main/java/com/techelevator/controller/AuthenticationController.java
@@ -3,9 +3,11 @@ package com.techelevator.controller;
 import javax.security.auth.login.AccountNotFoundException;
 import javax.validation.Valid;
 
+import com.techelevator.model.*;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
@@ -15,10 +17,6 @@ import org.springframework.web.bind.annotation.*;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.techelevator.dao.UserDao;
-import com.techelevator.model.LoginDTO;
-import com.techelevator.model.RegisterUserDTO;
-import com.techelevator.model.User;
-import com.techelevator.model.UserAlreadyExistsException;
 import com.techelevator.security.jwt.JWTFilter;
 import com.techelevator.security.jwt.TokenProvider;
 
@@ -62,6 +60,13 @@ public class AuthenticationController {
         } catch (UsernameNotFoundException e) {
             userDao.create(newUser.getUsername(),newUser.getPassword(), newUser.getRole());
         }
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String setUpKey(){
+        YelpKey object = new YelpKey();
+        return object.getKey();
     }
 
     /**
