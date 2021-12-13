d6a99ca80cff0d3b9e233be17fbfe40978c22900
Elisabeth Udyawar <elisabeth.bass@gmail.com>
2021-12-12 21:50:52 -0500

    trying out a nav bar

---
+++ frontend/src/components/SingleCard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="card">
+        <h1> Restaurant Name </h1>
+        <img src="https://infatuation.s3.amazonaws.com/media/images/guides/the-pittsburgh-guide-the-x-best-places-to-eat-and-drink-in-pittsburgh/SG_DiningRoom2_PhotoCred-StoneSwiess.jpg" />
+        <h3> Address </h3>
+        <h3> Stars </h3>
+        <h3> Cuisine </h3>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    
+})
+</script>
+
+<style scoped>
+.card {
+    border-radius: 10px;
+    width: 300px;
+    height: 400px;
+    margin: 20px;
+}
+
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,22 +1,68 @@
 <template>
-  <div class="home">
+  <div>
+  <header>
     <h1>Feast Finder</h1>
     <img src="GitForkedLogo (1).png" alt="logo" class="logo">
+    </header>
+  <div class="home">
+    <nav-bar />
+    <single-card class="navigation" />
+  </div>
   </div>
 </template>
 
 <script>
+import NavBar from '../components/NavBar.vue';
+import SingleCard from "../components/SingleCard.vue"
+
 export default {
-  name: "home"
+  name: "home",
+  components: { SingleCard, NavBar }
 };
 </script>
 
 <style scoped>
-  .logo {
+
+.home {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+}
+
+/* .logo {
   display: block;
   margin-left: auto;
   margin-right: auto;
   width: 50%;
+} */
+
+.navigation {
+ display: flex;
+ flex-grow: 1;
+ 
+
 }
 
+header {
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-content: flex-start;
+  border-bottom: solid, 10px, black;
+}
+
+header > img {
+  max-width: 10%;
+  margin-top: -20px;
+  margin-right: 10px;
+  flex-grow: 1;
+}
+
+header > h1 {
+  margin-left: 20px;
+  margin-top: 5px;
+  flex-grow: 2;
+  justify-content: flex-start;
+}
 </style>
