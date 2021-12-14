cbf1b3c476abad2bb3b8df49df0906bef3e1bbb3
Grant <grant.sapir@gmail.com>
2021-12-14 10:11:36 -0500

    cards

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="home">
     <h1>Feast Finder</h1>
-    <img src="GitForkedLogo (1).png" alt="logo" class="logo">
+   <b-container>
+      <b-row class="vh-100" align-v="center">
+        <b-col></b-col>
+        <b-col> <card/> </b-col>
+        <b-col></b-col>
+      </b-row>   
+   </b-container>    
   </div>
 </template>
 
 <script>
+import Card from '../components/card.vue';
+ 
 export default {
+  components: {Card},
   name: "home"
 };
 </script>
 
 <style scoped>
-  .logo {
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-  width: 50%;
-}
 
 </style>
