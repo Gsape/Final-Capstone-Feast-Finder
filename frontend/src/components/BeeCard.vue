5cc827b20ca0d867f266841089e0ac43d6c7d352
Elisabeth Udyawar <elisabeth.bass@gmail.com>
2021-12-14 22:58:20 -0500

    testing a bcard

---
+++ frontend/src/components/BeeCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div v-for="list in $store.state.searchResults" :key="list.businesses">
+    <b-card
+        v-for="restaurant in list.businesses" :key="restaurant.name"
+        title={{ restaurant.name }}
+        img-src='restaurant.image_url'
+        img-alt="Image"
+        img-top
+        tag="article"
+        style="max-width: 20rem;"
+        class="mb-2"
+    >
+        <b-card-text>
+        {{ restaurant.rating }}
+        </b-card-text>
+
+        <b-button href="#" variant="primary">Go somewhere</b-button>
+    </b-card>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
