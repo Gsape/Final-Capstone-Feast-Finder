2d0f016c2ee006b6c6dfc3a0ebd9414b33d4a537
Elisabeth Udyawar <elisabeth.bass@gmail.com>
2021-12-16 15:15:58 -0500

    instructions v-if

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,13 +2,26 @@
   <div>
   <header>
     <banner/>
-    </header>
-    <h1>Recommended Restaurants</h1>
-      <!-- <router-link :to="{ name: 'restaurant-details', params: { id: restaurant.id }}" v-for="r in $store.state.restaurant" v-bind:key="r.id"> -->
-        <single-card />
-      <!-- </router-link> -->
-    <h1>Your Favorite Restaurants</h1>
-    <fav-card />
+  </header>
+    <div v-if="this.$store.state.searchResults.length === 0" class="instructions">
+      <h1>Welcome to Feast Form!</h1>
+      <p>
+        <ul>This is your home page.</ul>
+        <ul>Right now, this page is as empty as your tummy.</ul>
+        <ul>To fill both, click the Feast Form button to select any of the foods you might be interested in eating today.</ul>
+        <ul>When you submit, you'll have a feast before you!</ul>
+        <ul>Hover over a card to see more details about that restaurant.</ul>
+        <ul>Click the "Like button" to save the restaurant as a favorite in your profile.</ul>
+      </p>
+    </div>
+    <div v-else>
+      <h1>Recommended Restaurants</h1>
+        <!-- <router-link :to="{ name: 'restaurant-details', params: { id: restaurant.id }}" v-for="r in $store.state.restaurant" v-bind:key="r.id"> -->
+          <single-card />
+        <!-- </router-link> -->
+      <h1>Your Favorite Restaurants</h1>
+      <fav-card />
+    </div>
   </div>
 </template>
 
@@ -26,7 +39,15 @@ export default {
 
 <style scoped>
 
+ul{
+  text-align: center;
+  font-size: 25px;
+}
 
+.instructions{
+  width: 60%;
+  margin: auto;
+}
 
 header > img {
   max-width: 10%;
