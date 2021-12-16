0efbd1b96f6d730b17b73816a0b7845518528d4d
Paige Wortmann <pagewortmann@gmail.com>
2021-12-16 14:22:09 -0500

    Flip cards works

--- frontend/src/components/dataTest.vue
+++ frontend/src/components/dataTest.vue
@@ -1,30 +1,15 @@
 <template>
   <div>
-    <!-- <h1>Restaurant</h1> -->
-    <!-- <h2>{{ restaurant }}</h2> -->
-    <!-- <h2>{{ restaurant }}</h2> -->
-    <!-- <h2>{{ restaurant.id }}</h2>
-    <h2>{{ restaurant.rating }}</h2> -->
+    <h1>Restaurant</h1>
+    <h2>{{ restaurant }}</h2>
+    <h2>{{ restaurant.name }}</h2>
 
-    <!-- <ul>
-      <li v-for="list in $store.state.searchResults" :key="list.businesses">
-          <p v-for="restaurant in list.businesses" :key="restaurant.name">
-              {{ restaurant.name }}
-          </p>
-      </li>
-    </ul> -->
-
-    <!-- <h1>Profile</h1> -->
-    <h1></h1>
-    <h1></h1>
-    <h1></h1>
-    <h1></h1>
   </div>
 </template>
 
 <script>
-// import yelpService from "../services/YelpService";
-import authService from "../services/AuthService";
+import yelpService from "../services/YelpService";
+// import authService from "../services/AuthService";
 
 
 export default {
@@ -32,33 +17,33 @@ export default {
   data() {
     return {
       businessID: '',
-      address: '',
-      responseJSON: {},
     };
   },
 
   //   gR9DTbKCvezQlqvD7_FzPw
 
   methods: {
-    getYelpToken(){
-      authService.yelpKey().then((response) => {
-        this.$store.commit("SET_YELP_TOKEN", response.data)});
-    }
-    // getRestaurant() {
-    //   yelpService
-    //     .getSingleRestaurant(this.$route.params.businessID) // this.$route.params.businessID
-    //     .then((response) => {
-    //         this.$store.commit("SET_RESTAURANT", response.data);
-    //         // this.$router.push('/')
-    //     })
-    //     .catch(error => {
-    //       if (error.response && error.response.status === 404) {
-    //         alert("noooo");
-    //         console.log(error.response.status)
-    //       }
-    //       // this.$router.push("/login");
-    //     });
+    // getYelpToken(){
+    //   authService.yelpKey().then((response) => {
+    //     this.$store.commit("SET_YELP_TOKEN", response.data)});
     // },
+
+
+    getRestaurant() {
+      yelpService
+        .getSingleRestaurant(this.$route.params.businessID) // this.$route.params.businessID
+        .then((response) => {
+            this.$store.commit("SET_RESTAURANT", response.data);
+            // this.$router.push('/')
+        })
+        .catch(error => {
+          if (error.response && error.response.status === 404) {
+            alert("noooo");
+            console.log(error.response.status)
+          }
+          // this.$router.push("/login");
+        });
+    },
 
     // submitRestaurant() {
     //     const newRestaurant = {
@@ -74,33 +59,30 @@ export default {
     // },
   },
 
-  // created() {
-  //   this.getRestaurant();
-  // },
+  created() {
+    this.getRestaurant();
+  },
 
   computed: {
     restaurant() {
       return this.$store.state.restaurant;
     },
 
-    JSONObject() {
-      return this.$store.state.responseJSON;
-    },
   // },
-  computed: {
-    cardsArray(){
-      let storeResults = this.$store.state.searchResults;
-      let returnedArray = [];
-      const size = storeResults.size;
-      for (let i = 0; i < size; i++){
-        let businessArray = storeResults[i].businesses;
-        businessArray.forEach((element) => {
-          returnedArray.unshift(element);
-        })
-      }
-      return returnedArray;
-    }
-  }
+  // computed: {
+  //   cardsArray(){
+  //     let storeResults = this.$store.state.searchResults;
+  //     let returnedArray = [];
+  //     const size = storeResults.size;
+  //     for (let i = 0; i < size; i++){
+  //       let businessArray = storeResults[i].businesses;
+  //       businessArray.forEach((element) => {
+  //         returnedArray.unshift(element);
+  //       })
+  //     }
+  //     return returnedArray;
+  //   }
+  // }
 }
 }
 
